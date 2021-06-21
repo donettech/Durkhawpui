bf5c42f6b28f0a51d16f74e14d4dbc6408a4400e
Xuala khawlhring <synxuala@gmail.com>
2021-06-22 01:00:33 +0530

    code comment cleanup

---
+++ lib/ui/home/homeRoot.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class HomeRoot extends StatefulWidget {
+  HomeRoot({Key key}) : super(key: key);
+
+  @override
+  _HomeRootState createState() => _HomeRootState();
+}
+
+class _HomeRootState extends State<HomeRoot> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(),
+    );
+  }
+}
